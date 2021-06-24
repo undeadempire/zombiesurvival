bc8736bd59e4ebcd5d08526ebf8ddfd25f4dfa77
LazySoltane <callumharris1997@yahoo.co.uk>
2021-06-24 08:36:10 +0100

    Suicide Tweak #2

--- entities/weapons/weapon_zs_suicidebomb/shared.lua
+++ entities/weapons/weapon_zs_suicidebomb/shared.lua
@@ -1,83 +1,135 @@
+AddCSLuaFile()
+
 SWEP.PrintName = "Suicide Bomb"
 SWEP.Description = "A pack of explosives that can be detonated killing yourself and zeds around you."
+SWEP.Slot = 4
+SWEP.SlotPos = 0
+
+if CLIENT then
+	SWEP.ViewModelFOV = 50
+	SWEP.BobScale = 0.5
+	SWEP.SwayScale = 0.5
+end
 
 SWEP.ViewModel = "models/weapons/v_pistol.mdl"
 SWEP.WorldModel = Model("models/weapons/w_c4_planted.mdl")
+SWEP.UseHands = true
 
-SWEP.AmmoIfHas = true
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = -1
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = "none"
 
-SWEP.Primary.ClipSize = 1
-SWEP.Primary.DefaultClip = 1
-SWEP.Primary.Ammo = "sniperpenetratedround"
-SWEP.Primary.Delay = 0.1
-SWEP.Primary.Automatic = true
-
-SWEP.Secondary.ClipSize = 1
-SWEP.Secondary.DefaultClip = 1
-SWEP.Secondary.Ammo = "dummy"
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic = false
+SWEP.Secondary.Ammo = "none"
 
 SWEP.WalkSpeed = SPEED_NORMAL
-SWEP.FullWalkSpeed = SPEED_SLOW
+
+SWEP.NoMagazine = true
+SWEP.Undroppable = true
+SWEP.NoPickupNotification = true
+
+SWEP.HoldType = "slam"
 
 SWEP.NoDeploySpeedChange = true
 
-if SERVER then
-    function SWEP:Think()
-        for _, ent in ent:GetOwner() == self:GetOwner() do
-            if ent:GetExplodeTime() == 0 then
-                ent:SetExplodeTime(CurTime() + ent.ExplosionDelay)
-                return
-            end
-        end
-    
-        self:GetOwner():StripWeapon(self:GetClass())
-    end
-    end
-
 function SWEP:Initialize()
-	self:SetWeaponHoldType("slam")
-	GAMEMODE:DoChangeDeploySpeed(self)
-	self:HideViewAndWorldModel()
+	self:SetWeaponHoldType(self.HoldType)
 end
 
-function SWEP:SetReplicatedAmmo(count)
-	self:SetDTInt(0, count)
-end
-
-function SWEP:GetReplicatedAmmo()
-	return self:GetDTInt(0)
-end
-
-function SWEP:GetWalkSpeed()
-	if self:GetPrimaryAmmoCount() > 0 then
-		return self.FullWalkSpeed
+if SERVER then
+function SWEP:Think()
+	for _, ent in pairs(ents.FindByClass("prop_detpack")) do
+		if ent:GetOwner() == self:GetOwner() then
+			return
+		end
 	end
+
+	self:GetOwner():StripWeapon(self:GetClass())
+end
+end
+
+function SWEP:PrimaryAttack()
+	self:SendWeaponAnim(ACT_SLAM_DETONATOR_DETONATE)
+
+	if CLIENT then return end
+
+	for _, ent in pairs(ents.FindByClass("prop_detpack")) do
+		if ent:GetOwner() == self:GetOwner() and ent:GetExplodeTime() == 0 then
+			ent:SetExplodeTime(CurTime() + ent.ExplosionDelay)
+		end
+	end
+end
+
+function SWEP:SecondaryAttack()
 end
 
 function SWEP:Reload()
-end
-
-function SWEP:CanPrimaryAttack()
-	if self:GetOwner():IsHolding() or self:GetOwner():GetBarricadeGhosting() then return false end
-
-	if self:GetPrimaryAmmoCount() <= 0 then
-		self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
-		return false
-	end
-
-	return true
-end
-
-function SWEP:CanSecondaryAttack()
 	return false
 end
 
 function SWEP:Deploy()
 	gamemode.Call("WeaponDeployed", self:GetOwner(), self)
 
+	self:SendWeaponAnim(ACT_SLAM_DETONATOR_IDLE)
+
 	return true
 end
 
 function SWEP:Holster()
+	return true
+end
+
+if not CLIENT then return end
+
+function SWEP:DrawWeaponSelection(x, y, w, h, alpha)
+	self:BaseDrawWeaponSelection(x, y, w, h, alpha)
+end
+
+function SWEP:Think()
+end
+
+function ENT:Explode()
+	if self.Exploded then return end
+	self.Exploded = true
+
+	local owner = self:GetOwner()
+	if owner:IsValidHuman() then
+		local pos = self:GetPos()
+
+		util.BlastDamagePlayer(self, owner, pos, 256, 480, DMG_ALWAYSGIB)
+
+		local effectdata = EffectData()
+			effectdata:SetOrigin(pos)
+			effectdata:SetNormal(self:GetUp() * -1)
+		util.Effect("decal_scorch", effectdata)
+
+		for i=1, 3 do
+			self:EmitSound("npc/env_headcrabcanister/explosion.wav", 75 + i * 5, 100)
+		end
+		for i=1, 2 do
+			ParticleEffect("dusty_explosion_rockets", pos, angle_zero)
+		end
+	end
+end
+
+function ENT:Think()
+	if self.Exploded then
+		self:Remove()
+		return
+	end
+
+	if self:GetExplodeTime() ~= 0 then
+		if CurTime() >= self:GetExplodeTime() then
+			self:Explode()
+		elseif self.NextBlip <= CurTime() then
+			self.NextBlip = CurTime() + 0.4
+			self:EmitSound("weapons/c4/c4_beep1.wav")
+		end
+	end
+
+	self:NextThink(CurTime())
 	return true
 end
