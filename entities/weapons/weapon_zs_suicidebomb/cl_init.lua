cbcf22cf292106e45f89cf177fedcfd677f23dd3
LazySoltane <callumharris1997@yahoo.co.uk>
2021-06-26 09:43:29 +0100

    Tweaking Delay and worldmodel so it looks sexier

--- entities/weapons/weapon_zs_suicidebomb/cl_init.lua
+++ entities/weapons/weapon_zs_suicidebomb/cl_init.lua
@@ -15,18 +15,3 @@ end
 function SWEP:Reload()
 	self.Owner:DropWeapon(self.Owner:GetActiveWeapon())
 end
-function SWEP:PrimaryAttack()
-	--if ( !self:CanPrimaryAttack() ) then return end
-	self.BaseClass.ShootEffects (self);
-	self:SendWeaponAnim( ACT_GMOD_TAUNT_SALUTE )
-	local explode = ents.Create( "env_explosion" )
-	explode:SetPos( self.Owner:GetPos() )
-	explode:SetOwner( self.Owner )
-	explode:Spawn()
-	explode:SetKeyValue( "iMagnitude", "340" )
-	explode:Fire( "Explode", 0, 0 )
-	explode:EmitSound( "weapon_AWP.Single", 800, 800 )
-	if self.Owner:Alive() then
-	self.Owner:Kill()
-	end
-end
